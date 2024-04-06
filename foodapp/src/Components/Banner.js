701001839faa9ea08a3afeab6f68fb0a851b5298
subhashprasad2001@gmail.com <96110162+subhashdippu@users.noreply.github.com>
2024-04-06 13:01:12 +0530

    Fifth

---
+++ foodapp/src/Components/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../App.css'
+const Banner = () => {
+    return (
+        <div className='section-container bg-gradient-to-r from-indigo-500 from-0% to-100%'>
+            <div className='py-24'>
+                Banner
+            </div>
+        </div>
+    )
+}
+
+export default Banner
--- foodapp/src/Components/Navbar.js
+++ foodapp/src/Components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { MdOutlineAddIcCall } from "react-icons/md";
 const Navbar = () => {
 
     const navItems = (
@@ -30,7 +30,7 @@ const Navbar = () => {
     );
     return (
         <div>
-            <div className="navbar bg-base-100">
+            <div className="navbar bg-neutral text-neutral-content">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -48,18 +48,37 @@ const Navbar = () => {
                             <li><a>Item 3</a></li>
                         </ul>
                     </div>
-                    <a href="/" className="btn btn-ghost text-xl">Logo</a>
+                    <a href="/" className="btn btn-ghost text-xl">
+                        {/* <img src="https://cdni.iconscout.com/illustration/premium/thumb/food-delivery-boy-4047433-3351728.png" alt="" /> */}
+                        Logo
+                    </a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
                         {navItems}
                     </ul>
                 </div>
+
                 <div className="navbar-end">
-                    <a className="btn">Button</a>
+                    {/* Search Logo */}
+                    <button className="btn btn-ghost btn-circle hidden lg:flex">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                    </button>
+
+                    {/* Cart */}
+                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle mr-3 lg:flex hidden items-center justify-center  " >
+                        <div className="indicator">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                            <span className="badge badge-sm indicator-item">8</span>
+                        </div>
+                    </div>
+
+                    <a className="btn bg-blue rounded-full px-6 text-white flex items-center gap-2">
+                        <MdOutlineAddIcCall />
+                    </a>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
